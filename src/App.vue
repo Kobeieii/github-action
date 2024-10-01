a5ea30052be8c4a6a5667ea69f1d4f73d39d08e0
Kobeieii <ploy.thitiworada@outlook.com>
2024-10-01 20:48:07 +0700

    fix(ga): edit hostname

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,12 @@
   <HelloWorld />
   <div class="mt-5 text-xs hover:text-base">
     <p>From {{ env }} server</p>
-    {{ hostname }}
   </div>
 </template>
 
 <script setup lang="ts">
 const hostname = new URL(import.meta.url).hostname
-const env = ref<string>(hostname === "github-action-demo-657305254360.asia-southeast1" ? "Production" : "Development")
+const env = ref<string>(hostname === "github-action-demo-657305254360.asia-southeast1.run.app" ? "Production" : "Development")
 </script>
 
 <style scoped>
